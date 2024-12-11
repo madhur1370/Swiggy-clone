5650eac13d1c66f17eee4000b865405c984c9f76
madhur <nadhur45v@gmail.com>
2024-12-11 15:25:12 +0530

    someback

--- backend/src/main/java/com/project/backend/config/HttpsServerConf.java
+++ backend/src/main/java/com/project/backend/config/HttpsServerConf.java
@@ -1,16 +1,16 @@
-//package com.project.backend.config;
-//
-//
-//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class HttpsServerConf {
-//    @Bean
-//    public ServletWebServerFactory serverFactory() {
-//        return new TomcatServletWebServerFactory();
-//    }
-//
-//}
+package com.project.backend.config;
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsServerConf {
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+}
--- backend/src/main/java/com/project/backend/mongoCon/Menu.java
+++ backend/src/main/java/com/project/backend/mongoCon/Menu.java
@@ -10,7 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Menu {
-    private  String dishname;
+    private  String dish_name;
     private  Integer price;
     private  List<Integer> rating;
     private  String image;
